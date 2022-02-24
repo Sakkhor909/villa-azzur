656234e6419584826232479d6a0fb650bad3f537
Sakkhor909 <minhajur.rahman.sakkhor@gmail.com>
2022-02-25 00:28:23 +0600

    Slider created

--- components/slider.jsx
+++ components/slider.jsx
@@ -1,19 +1,31 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
 import "swiper/css";
-import Image from "next/image";
+import "swiper/css/pagination";
 
 function Slider() {
   return (
     <>
-      <Swiper className="mySwiper">
-        <SwiperSlide>
-          <Image src="/food1.jpg" alt="Slider image" layout="fill" />
+      <Swiper
+        className="mySwiper"
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false
+        }}
+        pagination={{ clickable: true }}
+        modules={[Pagination, Autoplay]}
+      >
+        <SwiperSlide className="slideText image1">
+          <div className="text-1">Enjoy Our Food</div>
+          <div className="text-2">Only the best ingredients for our dishes</div>
         </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/food2.jpg" alt="Slider image" layout="fill" />
+        <SwiperSlide className="slideText image2">
+          <div className="text-1">Elegant Interior and Design</div>
+          <div className="text-2">High-class Professional Service</div>
         </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/food6.jpg" alt="Slider image" layout="fill" />
+        <SwiperSlide className="slideText image3">
+          <div className="text-1">Tradition & Passion</div>
+          <div className="text-2">Only the best ingredients for our dishes</div>
         </SwiperSlide>
       </Swiper>
     </>
